dbf1d09a00c6acaa7b41714f8c781be2a98db5fc
Alex <Alex@126.com>
2021-05-25 06:00:34 +0800

    first commit

---
+++ src/main/java/com/git/GitTest.java
@@ -0,0 +1,13 @@
+package com.git;
+
+/**
+ * @author Alex
+ * @create 2021-05-25 5:56
+ */
+public class GitTest {
+
+    public static void main(String[] args) {
+        System.out.println("hello git!");
+    }
+
+}
